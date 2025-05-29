876dc9bb3d63660b698038c3cf321855690cbada
Kabelo Matlakala <matlakalakabelo1@gmail.com>
2025-05-29 21:25:48 +0200

    update ...

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,17 @@
-// tailwind.config.js
+/** @type {import('tailwindcss').Config} */
 module.exports = {
     content: [
-      './app/**/*.{js,ts,jsx,tsx}',
-      './components/**/*.{js,ts,jsx,tsx}',
-      './pages/**/*.{js,ts,jsx,tsx}',
+      './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+      './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+      './src/app/**/*.{js,ts,jsx,tsx,mdx}',
     ],
     theme: {
-      extend: {},
+      extend: {
+        fontFamily: {
+          sans: ['var(--font-sans)'],
+          mono: ['var(--font-mono)'],
+        },
+      },
     },
     plugins: [],
   }
