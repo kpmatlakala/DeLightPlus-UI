bd17b1dab61a5187f1b8bd95ba203b45bb6210c5
Kabelo Matlakala <matlakalakabelo1@gmail.com>
2025-09-30 18:38:06 +0200

    update | release 0.1.34

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/**/*.{js,ts,jsx,tsx}',
-    // './node_modules/delightplus-ui/dist/**/*.{js,ts,jsx,tsx}',
+    './src/**/*.{js,ts,jsx,tsx}', 
   ],
   theme: {
     extend: {},
