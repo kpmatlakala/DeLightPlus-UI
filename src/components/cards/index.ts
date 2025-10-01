3512996c99ba82d4d7886fb6a584e2b75a297238
Kabelo Matlakala <matlakalakabelo1@gmail.com>
2025-10-01 18:59:57 +0200

    ...

--- src/components/cards/index.ts
+++ src/components/cards/index.ts
@@ -1,5 +1,6 @@
 // components/cards/index.ts
 
+export { ProjectCard } from './ProjectCard';
 export { default as ClockCard } from './ClockCard';
 export { default as WeatherCard } from './WeatherCard';
 export { default as SocialProfileCard } from './SocialProfileCard';
